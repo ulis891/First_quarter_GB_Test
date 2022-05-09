0f4b4ffb43a9e70da7a6247c069a9579e571a2bf
Aleksandr Kostromin <ulis89@yandex.ru>
2022-05-09 09:55:25 +0300

    upd README.md

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿// Итоговая проверочная работа
 
 string[] wordArray = {"hello", "2", "world", "The", "1234", "1567","sun", "had", "consulting", "journal",
-                     "set", "Mark", "but", "was", "computer science", "not", "out", "Russia", "yet", "-2", 
+                     "set", "Mark", "but", "was", "computer science", "not", "out", "Russia", "yet", "-2",
                      ":-)", "Denmark", "Kazan"};
 
 // Метод для поиска сторк с колличеством символов меньше или раным 3
-string[] FindShortStrings (string[] array)
+string[] FindShortStrings(string[] array)
 {
-    int index = 0;  // Инициализируем переменную для подсчёта колличества искомых строк
+    // Инициализируем переменную для подсчёта колличества искомых строк
+    int index = 0;  
 
     // Поиск и подсчёт искомых строк
     for (int i = 0; i < array.Length; i++)
@@ -15,25 +16,27 @@ string[] FindShortStrings (string[] array)
         if (array[i].Length <= 3) index++;
     }
 
-    string[] shortWordArray = new string [index];  // Инициализируем массив для записи в него искомых строк
-    index = 0;  // Обнуляем переменную index для использования при индексировании нового массива
+    // Инициализируем массив для записи в него искомых строк
+    string[] shortStringsArray = new string[index];
+    // Обнуляем переменную index для использования при индексировании нового массива
+    index = 0;
 
     // Заполнение нового масиваа искомыми строками
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
-        {   
-            shortWordArray[index]= array[i];
+        {
+            shortStringsArray[index] = array[i];
             index++;
         }
     }
-    return shortWordArray;
+    return shortStringsArray;
 }
 
 
 
 
-void PrintArray (string[] array)
+void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
