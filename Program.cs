9142b3537f57fffac61debefd29fef42c4211f56
Aleksandr Kostromin <ulis89@yandex.ru>
2022-05-09 10:42:18 +0300

    upd new program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,31 @@
 ﻿// Итоговая проверочная работа
 
-string[] wordArray = {"hello", "2", "world", "The", "1234", "1567","sun", "had", "consulting", "journal",
+string[] stringsArray = {"hello", "2", "world", "The", "1234", "1567","sun", "had", "consulting", "journal",
                      "set", "Mark", "but", "was", "computer science", "not", "out", "Russia", "yet", "-2",
                      ":-)", "Denmark", "Kazan"};
 
-// Метод для поиска сторк с колличеством символов меньше или раным 3 и формирования из них нового массива
-string[] FindShortStrings(string[] array)
+// Метод для поиска сторк с колличеством символов меньше или раным 3
+int CountShortStrings(string[] array)
 {
     // Инициализируем переменную для подсчёта колличества искомых строк
-    int index = 0;  
+    int count = 0;  
 
     // Поиск и подсчёт искомых строк
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i].Length <= 3) index++;
+        if (array[i].Length <= 3) count++;
     }
+    return count;
+}
 
+// Метод для формирования и заполнения нового массива с колличеством символов меньше или раным 3
+string[] FillShortStringsArray (int shortStringsNumber, string[] array)
+{
     // Инициализируем массив для записи в него искомых строк
-    string[] shortStringsArray = new string[index];
+    string[] shortStringsArray = new string[shortStringsNumber];
 
-    // Обнуляем переменную index для использования при индексировании нового массива
-    index = 0;
+    // Инициализируем переменную для использования при индексировании нового массива
+    int index = 0;
 
     // Заполнение нового масиваа искомыми строками
     for (int i = 0; i < array.Length; i++)
@@ -47,4 +52,4 @@ void PrintArray(string[] array)
 }
 
 
-PrintArray(FindShortStrings(wordArray));
+PrintArray(FillShortStringsArray(CountShortStrings(stringsArray), stringsArray));
