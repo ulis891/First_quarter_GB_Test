2a06b9ca574bf6a4bc55bdfa2d3156f032d209a4
Aleksandr Kostromin <ulis89@yandex.ru>
2022-05-09 10:17:58 +0300

    upd program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ string[] wordArray = {"hello", "2", "world", "The", "1234", "1567","sun", "had",
                      "set", "Mark", "but", "was", "computer science", "not", "out", "Russia", "yet", "-2",
                      ":-)", "Denmark", "Kazan"};
 
-// Метод для поиска сторк с колличеством символов меньше или раным 3
+// Метод для поиска сторк с колличеством символов меньше или раным 3 и формирования из них нового массива
 string[] FindShortStrings(string[] array)
 {
     // Инициализируем переменную для подсчёта колличества искомых строк
@@ -18,7 +18,7 @@ string[] FindShortStrings(string[] array)
 
     // Инициализируем массив для записи в него искомых строк
     string[] shortStringsArray = new string[index];
-    
+
     // Обнуляем переменную index для использования при индексировании нового массива
     index = 0;
 
@@ -31,6 +31,7 @@ string[] FindShortStrings(string[] array)
             index++;
         }
     }
+    // Возвращаем массив сформированный из строк с колличеством символов меньше или раным 3
     return shortStringsArray;
 }
 
