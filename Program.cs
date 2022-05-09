344c55b0aa4877bfeecdc259045e2d4f30e32e84
Aleksandr Kostromin <ulis89@yandex.ru>
2022-05-09 11:41:42 +0300

    upd Test_FQ_GB_algorithm.drawio and program.cs

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@ string[] stringsArray = {"hello", "2", "world", "The", "1234", "1567","sun", "ha
                      "set", "Mark", "but", "was", "computer science", "not", "out", "Russia", "yet", "-2",
                      ":-)", "Denmark", "Kazan"};
 
-// Метод для поиска сторк с колличеством символов меньше или раным 3
+// Метод для поиска строк с количеством символов меньше или равным 3
 int CountShortStrings(string[] array)
 {
-    // Инициализируем переменную для подсчёта колличества искомых строк
+    // Инициализируем переменную для подсчёта количества искомых строк
     int count = 0;  
 
     // Поиск и подсчёт искомых строк
@@ -18,7 +18,7 @@ int CountShortStrings(string[] array)
     return count;
 }
 
-// Метод для формирования и заполнения нового массива с колличеством символов меньше или раным 3
+// Метод для формирования и заполнения нового массива с количеством символов меньше или равным 3
 string[] FillShortStringsArray (int shortStringsNumber, string[] array)
 {
     // Инициализируем массив для записи в него искомых строк
@@ -27,7 +27,7 @@ string[] FillShortStringsArray (int shortStringsNumber, string[] array)
     // Инициализируем переменную для использования при индексировании нового массива
     int index = 0;
 
-    // Заполнение нового масиваа искомыми строками
+    // Заполнение нового массива искомыми строками
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
@@ -36,20 +36,8 @@ string[] FillShortStringsArray (int shortStringsNumber, string[] array)
             index++;
         }
     }
-    // Возвращаем массив сформированный из строк с колличеством символов меньше или раным 3
+    // Возвращаем массив сформированный из строк с количеством символов меньше или равным 3
     return shortStringsArray;
 }
 
-
-
-
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-}
-
-
-PrintArray(FillShortStringsArray(CountShortStrings(stringsArray), stringsArray));
+FillShortStringsArray(CountShortStrings(stringsArray), stringsArray);
